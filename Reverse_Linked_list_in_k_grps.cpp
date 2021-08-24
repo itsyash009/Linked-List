3d97289a2e253d3593d6a0a2a3503073ddfd4464
Yash Baranwal <72246271+itsyash009@users.noreply.github.com>
2021-08-24 18:15:12 +0530

    Create Reverse_Linked_list_in_k_grps.cpp

---
+++ Reverse_Linked_list_in_k_grps.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        stack<node*> s;
+        node* current=head;
+        node* prev=NULL;
+        while(current!=NULL)
+        {
+            int c=0;
+            while(current!=NULL&&c<k)
+            {
+                s.push(current);
+                c++;
+                current=current->next;
+          
+            }
+        while(s.size()>0)
+        {
+            if(prev==NULL)
+            {
+                prev=s.top();
+                head=prev;
+                s.pop();
+            }
+            else
+            {
+                prev->next=s.top();
+                prev=prev->next;
+                s.pop();
+            }
+        }
+    }
+    prev->next=NULL;
+    return(head);
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
